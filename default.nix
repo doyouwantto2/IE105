52aca87f16eadd5f509cef7cfc8680a0091952b8
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-13 19:28:50 +0700

    fix: using pip rather than native nixos

--- default.nix
+++ default.nix
@@ -1,31 +1,31 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
 
-let
-  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-    langchain
-    langgraph
-    langsmith
-    langchain-ollama
-    pytest
-    numpy
-    ollama           
-    pandas
-    matplotlib
-  ]);
-
-in
 pkgs.mkShell {
   name = "IE105";
 
-  buildInputs = with pkgs; [
-    pythonEnv
-    ollama           
+  packages = with pkgs; [
+    python312
+    python312Packages.virtualenv
 
-    curl
+    ollama
     git
     ripgrep
   ];
 
   shellHook = ''
+    if [ ! -d .venv ]; then
+      virtualenv --no-setuptools --no-wheel .venv
+    fi
+
+    source .venv/bin/activate
+
+    if [ -f requirements.txt ]; then
+      STAMP=.venv/.requirements.stamp
+
+      if [ ! -f "$STAMP" ] || [ requirements.txt -nt "$STAMP" ]; then
+        python -m pip install -r requirements.txt
+        touch "$STAMP"
+      fi
+    fi
   '';
 }
